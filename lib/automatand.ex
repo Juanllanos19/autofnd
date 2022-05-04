bbf1a08429f92b3de08be4b3fac055fe3a4322e8
juan carlos llanos <o.llanos.juan19@gmail.com>
2022-05-04 14:58:58 -0500

    Terminando primer pregunta

--- lib/automatand.ex
+++ lib/automatand.ex
@@ -8,20 +8,63 @@ defmodule Automatand do
   defp automatasetter(_, [], acc), do: acc
   defp automatasetter(x, [h|t], acc), do: automatasetter(x,t, [[x|h] | acc])
 
-  def determinize(n) do
+
+
+  def prune(n) do
+    inicio = n.istate
+    delta = validT(n, inicio, [], Map.new())
+    |> List.flatten()
+    |> merge()
+
+    estados = Enum.map(delta, fn {{state1, _}, state2} -> [state1] ++ [state2] end)
+    |> Enum.concat()
+    |> Enum.uniq()
+    |> Enum.sort()
+
+    finales = Enum.filter(n.fstates, fn final -> final in estados end)
+    |> Enum.uniq()
+
+    %{%{%{n|delta: delta}|states: estados}|fstates: finales}
+  end
+
+  defp merge([]), do: Map.new()
+  defp merge([h|t]) do
+    Map.merge(h, merge(t))
+  end
+
+  defp validT(n, current, visited, delta) do
+    visited = visited ++ [current]
+    n.delta
+    |> Enum.filter(fn {{child, _}, _} -> child == current end)
+    |> Enum.map(fn {{current, simbolo}, susesor} ->
+      if(susesor not in visited) do
+        validT(n, susesor, visited, Map.put(delta, {current, simbolo}, susesor))
+      else
+        Map.put(delta, {current, simbolo}, susesor)
+      end
+    end)
+  end
+
+  def deltaput(n) do
     estados = automatasetter(n.states)
+    for estado <- estados, transicion <- n.alpha do
+      {{estado, transicion},
+        Enum.map(estado, fn conjunto -> n.delta[{conjunto, transicion}] end)
+        |> List.flatten
+      }
+    end
+  end
+
+  def determinize(n) do
+    deltav= deltaput(n)
     %{
       alpha: n.alpha,
       states: n.states,
       istate: n.istate,
       fstates: n.fstates,
-      delta: for estado <- estados, transicion <- n.alpha do
-      {{estado, transicion},
-        Enum.map(estado, fn conjunto -> n.delta[{conjunto, transicion}] end)
-        |> List.flatten
-      }
-      end
+      delta: deltav
     }
+
   end
 
   #def e_clousure(n, {}) do
